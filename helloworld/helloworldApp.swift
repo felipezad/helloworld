482aba625394de8fea6fa1dabf83c59e8ed55445
Felipe Zadinello <felipe.zadinello@blacklane.com>
2025-06-27 14:12:02 +0200

    Setup project

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Felipe Zadinello on 27.06.25.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ helloworldTests/helloworldTests.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldTests.swift
+//  helloworldTests
+//
+//  Created by Felipe Zadinello on 27.06.25.
+//
+
+import Testing
+@testable import helloworld
+
+struct helloworldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
